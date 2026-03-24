a9f25fa7a8bd0bf34251e42e1076698c6bb5ee5f
Bell Bell <72820380+BeLL-Umbr3llA@users.noreply.github.com>
2026-03-24 16:35:25 +0630

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:5000/api' || 'https://tu-showcase-admin.vercel.app/api';
 let allProjects = [];
 let editMode = false;
 let currentEditId = null;
